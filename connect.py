e8c4f03d97ec44a96de57016cb8e64dae96eec95
Tarun Govindarajan <tarunraj.gmt@gmail.com>
2023-10-17 15:11:46 -0500

    update

--- connect.py
+++ connect.py
@@ -10,34 +10,37 @@
 import numpy as np
 import asyncio
 from bleak import BleakClient
+import array
+import numpy as np
+import time
 
-'''
-# Check conenction
-async def main():
-    address = "ABCDEFG1-XXXX-XXXX-XXXX-XXXXXXXXXX"
 
-    async with BleakClient(address) as client:
-        print(client.is_connected) # prints True or False
-'''
+def byte_to_float(bytearray):
+    arr = array.array('f')
+    arr.frombytes(bytearray)
+    lst = list(arr)
 
-def encode_bytes_to_string(bytes_):
-    return
+    return lst[0]
 
-# address = "17CAE466-EE0D-6B2D-67D6-3271350BA08A"
 address= "78:77:B8:1B:B8:07"
 BLE_UUID_FLOW_CHAR = "00002a19-0000-1000-8000-00805f9b34fb" # need to replace using scanner.py
 
+
 async def main():
+
+    flow_data = np.array()
+
     # Connect to the Bluetooth device
     async with BleakClient(address) as client:
         # Check if connection was successful
-        print(f"Client connection: {client.is_connected}") # prints True or False
+        print(f"Client connection: {client.is_connected}") # prints True or Falseimport array
+
 
         while True:
             # Read the Flow Rate
             flow_rate = await client.read_gatt_char(BLE_UUID_FLOW_CHAR)
+            flow_rate_float = byte_to_float(flow_rate)
 
-            print(flow_rate)
-            # print(int.from_bytes(flow_rate, byteorder='big'))
+            np.concatenate([flow_data, np.array([time.time(), flow_rate_float])])
 
 asyncio.run(main())
