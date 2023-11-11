bdd4d1749133ee83e98d872df451da3ee70282a7
Tarun Govindarajan <tarunraj.gmt@gmail.com>
2023-11-10 22:35:28 -0600

    update arduino

--- arduino_serial_interface/arduino_serial_interface.ino
+++ arduino_serial_interface/arduino_serial_interface.ino
@@ -5,7 +5,7 @@ uint32_t lastMicros = 0;
 float valA;
 float filt_valA;
 
-#define INTERVAL 5000
+#define INTERVAL 5000  // sets the sampling rate for the sensor 
 
 
 template <int order> // order is 1 or 2
@@ -13,78 +13,78 @@ class LowPass
 {
   private:
     float a[order];
-    float b[order+1];
+    float b[order + 1];
     float omega0;
     float dt;
     bool adapt;
     float tn1 = 0;
-    float x[order+1]; // Raw values
-    float y[order+1]; // Filtered values
+    float x[order + 1]; // Raw values
+    float y[order + 1]; // Filtered values
 
-  public:  
-    LowPass(float f0, float fs, bool adaptive){
+  public:
+    LowPass(float f0, float fs, bool adaptive) {
       // f0: cutoff frequency (Hz)
       // fs: sample frequency (Hz)
       // adaptive: boolean flag, if set to 1, the code will automatically set
       // the sample frequency based on the time history.
-      
-      omega0 = 6.28318530718*f0;
-      dt = 1.0/fs;
+
+      omega0 = 6.28318530718 * f0;
+      dt = 1.0 / fs;
       adapt = adaptive;
       tn1 = -dt;
-      for(int k = 0; k < order+1; k++){
+      for (int k = 0; k < order + 1; k++) {
         x[k] = 0;
-        y[k] = 0;        
+        y[k] = 0;
       }
       setCoef();
     }
 
-    void setCoef(){
-      if(adapt){
-        float t = micros()/1.0e6;
+    void setCoef() {
+      if (adapt) {
+        float t = micros() / 1.0e6;
         dt = t - tn1;
         tn1 = t;
       }
-      
-      float alpha = omega0*dt;
-      if(order==1){
-        a[0] = -(alpha - 2.0)/(alpha+2.0);
-        b[0] = alpha/(alpha+2.0);
-        b[1] = alpha/(alpha+2.0);        
+
+      float alpha = omega0 * dt;
+      if (order == 1) {
+        a[0] = -(alpha - 2.0) / (alpha + 2.0);
+        b[0] = alpha / (alpha + 2.0);
+        b[1] = alpha / (alpha + 2.0);
       }
-      if(order==2){
-        float alphaSq = alpha*alpha;
+      if (order == 2) {
+        float alphaSq = alpha * alpha;
         float beta[] = {1, sqrt(2), 1};
-        float D = alphaSq*beta[0] + 2*alpha*beta[1] + 4*beta[2];
-        b[0] = alphaSq/D;
-        b[1] = 2*b[0];
+        float D = alphaSq * beta[0] + 2 * alpha * beta[1] + 4 * beta[2];
+        b[0] = alphaSq / D;
+        b[1] = 2 * b[0];
         b[2] = b[0];
-        a[0] = -(2*alphaSq*beta[0] - 8*beta[2])/D;
-        a[1] = -(beta[0]*alphaSq - 2*beta[1]*alpha + 4*beta[2])/D;      
+        a[0] = -(2 * alphaSq * beta[0] - 8 * beta[2]) / D;
+        a[1] = -(beta[0] * alphaSq - 2 * beta[1] * alpha + 4 * beta[2]) / D;
       }
     }
 
-    float filt(float xn){
+    float filt(float xn) {
       // Provide me with the current raw value: x
       // I will give you the current filtered value: y
-      if(adapt){
-        setCoef(); // Update coefficients if necessary      
+      if (adapt) {
+        setCoef(); // Update coefficients if necessary
       }
       y[0] = 0;
       x[0] = xn;
       // Compute the filtered values
-      for(int k = 0; k < order; k++){
-        y[0] += a[k]*y[k+1] + b[k]*x[k];
+      for (int k = 0; k < order; k++) {
+        y[0] += a[k] * y[k + 1] + b[k] * x[k];
       }
-      y[0] += b[order]*x[order];
+      y[0] += b[order] * x[order];
 
       // Save the historical values
-      for(int k = order; k > 0; k--){
-        y[k] = y[k-1];
-        x[k] = x[k-1];
+      for (int k = order; k > 0; k--) {
+        y[k] = y[k - 1];
+        x[k] = x[k - 1];
       }
-  
-      // Return the filtered value    
+
+      // Return the filtered value
       return y[0];
     }
 };
@@ -96,31 +96,31 @@ class LowPass
 // fs: sample frequency (Hz) (second arg)
 // adaptive: boolean flag, if set to 1, the code will automatically set the sample frequency based on the time history.(third arg)
 
-LowPass<2> lp(3,200,true);
+LowPass<2> lp(3, 200, true);
 
 
 void setup() {
-  Serial.begin(115200);      
+  Serial.begin(115200);
 }
 
 
 void loop() {
 
-  if (micros() - lastMicros > INTERVAL){
+  if (micros() - lastMicros > INTERVAL) {
     // Read the analog sensor value
-    valA = (analogRead(sensorPinA) / 1023.0) * 3.3;
+    //    valA = (analogRead(sensorPinA) / 1023.0) * 3.3;
 
-//    valA = float(random(8, 12))/10.0;
-    
+    valA = float(random(8, 12)) / 10.0;
+
     // Compute the filtered signal
     filt_valA = lp.filt(valA);
 
     // Update time
-    lastMicros = micros(); 
-    
+    lastMicros = micros();
+
     // Transmit the sensor reading as binary data
-    Serial.write((byte*)&filt_valA, sizeof(filt_valA));  
-//    Serial.println(valA);
-    }
- 
+    Serial.write((byte*)&filt_valA, sizeof(filt_valA));
+    //    Serial.println(valA);
+  }
+
 }
