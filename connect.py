a6ddbf5f63744f25f0d0c85c2fa9e766effdd409
Ji Hwan Park <jihwan98@cs.utexas.edu>
2023-12-08 14:37:12 -0600

    Updated saving csv

--- connect.py
+++ connect.py
@@ -25,8 +25,9 @@ def byte_to_float(bytearray):
     return lst[0]
 
 # address = "78:77:B8:1B:B8:07" # Windows
-address = "17CAE466-EE0D-6B2D-67D6-3271350BA08A" # Mac
-BLE_UUID_FLOW_CHAR = "00002a19-0000-1000-8000-00805f9b34fb" # need to replace using scanner.py
+address = "20D6F5E0-C9F0-3C62-B777-29A63C142F84" # Mac
+BLE_UUID_FLOW_CHAR_MOUTH = "00002a19-0000-1000-8000-00805f9b34fb" # need to replace using scanner.py
+BLE_UUID_FLOW_CHAR_NOSE = "00002a21-0000-1000-8000-00805f9b34fb" # need to replace using scanner.py
 
 async def main():
 
@@ -41,42 +42,19 @@ async def main():
     async with BleakClient(address) as client:
         # Check if connection was successful
         print(f"Client connection: {client.is_connected}\n")
-
-        # To-Do:
-        # 1) Add control switch
-        try:
-            while True:
-                # Read the Flow Rate
-                flow_rate = await client.read_gatt_char(BLE_UUID_FLOW_CHAR)
-                flow_rate_float = byte_to_float(flow_rate)
-                print(f"Received float data: {flow_rate_float}")
-                data = np.array([time.time(), flow_rate_float]).reshape(1,-1)
-                if len(flow_data) == 0:
-                    flow_data = data
-                else:
-                    flow_data = np.concatenate([flow_data, data])
-                i += 1
-                print(flow_data.shape)
-                # import pdb; pdb.set_trace()
-                plt.clf()
-
-                n_pts = len(flow_data)
-                if n_pts < 20:
-                    plt.plot(np.arange(n_pts), flow_data[-20:, 1], color='blue')
-                else:
-                    plt.plot(np.arange(n_pts-20, n_pts), flow_data[-20:, 1], color='blue')
-
-                print(np.arange(n_pts-20, n_pts))
-                plt.xlabel('Index')
-                plt.ylabel('Flowrate [m/s]')
-                plt.title('Real-time Flowrate Tracking')
-                plt.pause(0.001)
-                # if i > 10:
-                #     break
-        except KeyboardInterrupt:
-            # 2) Real-time Plot
-            print(flow_data.shape) 
-            savetxt(f'{time.time()}.csv', flow_data, delimiter=',') # csv log saver
-            return
+        start = time.time()
+        while (time.time() - start) < 5:
+            # Read the Flow Rate
+            flow_rate_mouth = await client.read_gatt_char(BLE_UUID_FLOW_CHAR_MOUTH)
+            flow_rate_nose  = await client.read_gatt_char(BLE_UUID_FLOW_CHAR_NOSE)
+            flow_rate_float_mouth = byte_to_float(flow_rate_mouth)
+            flow_rate_float_nose = byte_to_float(flow_rate_nose)
+            data = np.array([time.time(), flow_rate_float_mouth, flow_rate_float_nose]).reshape(1,-1)
+            if len(flow_data) == 0:
+                flow_data = data
+            else:
+                flow_data = np.concatenate([flow_data, data])
+        print("Saving ... ")
+        savetxt(f'{time.time()}.csv', flow_data, delimiter=',') # csv log saver
 
 asyncio.run(main())
